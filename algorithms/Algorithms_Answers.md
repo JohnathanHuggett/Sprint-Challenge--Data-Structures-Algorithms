d8ce3bc027da41ca510ab4e8fb414b1ab9a33d3d
JohnathanHuggett <Johnathan.Huggett@gmail.com>
2018-05-11 13:44:18 -0400

    Johnathan Huggett, completed sprint

--- algorithms/Algorithms_Answers.md
+++ algorithms/Algorithms_Answers.md
@@ -1,1 +1,17 @@
 Add your answers to the Algorithms exercises here.
+
+I
+a. O(n^3)
+b. O(n)
+c. O(log(sqrt(n)))
+d. O(n(logn))
+e. O(n^(n^n)
+f. O(n)
+g. O(n)
+
+II
+
+III
+a. since you start with an unbalanced you'll be in the worst case which would be o(n^2). Which is caused from each recursive call processes a list of size one less than the previous list.
+
+b. This would give you the best case which would be O(n log n). Then the recursive call processes only has look through a list of half the size.
--- data_structures/src/heap.js
+++ data_structures/src/heap.js
@@ -1,71 +1,85 @@
-const heapsort = (arr) => {
-  /* Your code here */
-  
+const heapsort = arr => {
+    // create a new heap
+    const heap = new Heap();
+
+    // loop through the arr and use the .insert method to put each element in the heap
+    arr.forEach(item => heap.insert(item));
+
+    // arr to hold the data once it is sorted
+    const sortedArr = [];
+
+    // while the heap's size is greater than 0 use .delete => which returns the highest number in the heap and reorder the heap to maintain its integrity
+    while (heap.getSize()) {
+        // take the value returned .delete and put it at the beginning of the sorted array
+        sortedArr.unshift(heap.delete());
+    }
+    // return the sorted array
+    return sortedArr;
 };
 
 class Heap {
-  constructor() {
-    this.storage = [null];
-    this.size = 0;
-  }
-
-  insert(val) {
-    const index = this.storage.push(val) - 1;
-    this.size++;
-    this.bubbleUp(index);
-  }
-
-  delete() {
-    if (this.storage.length === 2) {
-      this.size--;
-      return this.storage.pop();
-    } else if (this.storage.length === 1) {
-      return this.storage[0];
+    constructor() {
+        this.storage = [null];
+        this.size = 0;
     }
-    this.size--;
-    const max = this.storage[1];
-    this.storage[1] = this.storage.pop();
-    this.siftDown(1);
-    return max;
-  }
 
-  getMax() {
-    return this.storage[1];
-  }
-
-  getSize() {
-    return this.size;
-  }
-
-  bubbleUp(index) {
-    const parent = Math.floor(index/2);
-    if (parent > 0 && this.storage[parent] < this.storage[index]) {
-      [this.storage[parent], this.storage[index]] = [this.storage[index], this.storage[parent]];
-      this.bubbleUp(parent);
+    insert(val) {
+        const index = this.storage.push(val) - 1;
+        this.size++;
+        this.bubbleUp(index);
     }
-  }
 
-  siftDown(index) {
-    const child1 = index * 2;
-    const child2 = index * 2 + 1;
-    let maxChild;
-
-    if (this.storage[child1] !== undefined) {
-      if (this.storage[child2] === undefined) {
-        maxChild = child1;
-      } else if (this.storage[child2] !== undefined) {
-        maxChild = this.storage[child1] > this.storage[child2] ? child1 : child2;
-      }
-
-      if (this.storage[index] < this.storage[maxChild]) {
-        [this.storage[maxChild], this.storage[index]] = [this.storage[index], this.storage[maxChild]];
-        this.siftDown(maxChild);
-      }
+    delete() {
+        if (this.storage.length === 2) {
+            this.size--;
+            return this.storage.pop();
+        } else if (this.storage.length === 1) {
+            return this.storage[0];
+        }
+        this.size--;
+        const max = this.storage[1];
+        this.storage[1] = this.storage.pop();
+        this.siftDown(1);
+        return max;
     }
-  }
+
+    getMax() {
+        return this.storage[1];
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    bubbleUp(index) {
+        const parent = Math.floor(index / 2);
+        if (parent > 0 && this.storage[parent] < this.storage[index]) {
+            [this.storage[parent], this.storage[index]] = [this.storage[index], this.storage[parent]];
+            this.bubbleUp(parent);
+        }
+    }
+
+    siftDown(index) {
+        const child1 = index * 2;
+        const child2 = index * 2 + 1;
+        let maxChild;
+
+        if (this.storage[child1] !== undefined) {
+            if (this.storage[child2] === undefined) {
+                maxChild = child1;
+            } else if (this.storage[child2] !== undefined) {
+                maxChild = this.storage[child1] > this.storage[child2] ? child1 : child2;
+            }
+
+            if (this.storage[index] < this.storage[maxChild]) {
+                [this.storage[maxChild], this.storage[index]] = [this.storage[index], this.storage[maxChild]];
+                this.siftDown(maxChild);
+            }
+        }
+    }
 }
 
 module.exports = {
-  Heap,
-  heapsort,
+    Heap,
+    heapsort,
 };
